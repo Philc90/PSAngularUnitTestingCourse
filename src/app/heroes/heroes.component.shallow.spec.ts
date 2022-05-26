d40a6ebf02706499ca7b6c2bf65a822333758a75
Philip Chiang <philipchiang5@gmail.com>
2022-05-26 10:02:15 -0700

    finish 4. Shallow Integration tests. mocking child components

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,13 +1,24 @@
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
+import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 
+// HeroesComponent has 2 collaborators: HeroService and HeroComponent
 describe('HeroesComponent (shallow tests)', () => {
   let fixture:  ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>'
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+  }
 
   beforeEach(() => {
     HEROES = [
@@ -18,11 +29,14 @@ describe('HeroesComponent (shallow tests)', () => {
 
     mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [
+        HeroesComponent,
+        FakeHeroComponent
+      ],
       providers: [
         { provide: HeroService, useValue: mockHeroService }
       ],
-      schemas: [NO_ERRORS_SCHEMA]
+      // schemas: [NO_ERRORS_SCHEMA]
     })
     fixture = TestBed.createComponent(HeroesComponent)
   });
@@ -33,5 +47,13 @@ describe('HeroesComponent (shallow tests)', () => {
     fixture.detectChanges();
 
     expect(fixture.componentInstance.heroes.length).toBe(3);
+  })
+
+  // mocking child component
+  it('should create one li for each hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES)) // whenever getHeroes is called, i.e. in heroes.ngOnInit, use mock
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
   })
 });
